0b37aa395405eef578d3774c4d836531451a46ec
isksss <isksss.prp@gmail.com>
2022-11-20 02:57:11 +0900

    ファイル編成変更

---
+++ src/js/static.js
@@ -0,0 +1,16 @@
+// 固定ファイル
+Vue.createApp({
+    data(){
+        return {
+            current: new Date()
+        }
+    },
+    created(){
+        this.timer = setInterval(()=>{
+            this.current = new Date()
+        }, 1000);
+    },
+    beforeUnmount(){
+        clearInterval(this.timer);
+    }
+}).mount('#header');
